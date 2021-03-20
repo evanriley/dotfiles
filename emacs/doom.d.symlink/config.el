af0a1c3c3cbfa98d51f075a6bf7096e374202a27
Evan Riley <evanriley@hey.com>
2021-03-20 00:30:24 -0400

    Clean up doom emacs config

--- emacs/doom.d.symlink/config.el
+++ emacs/doom.d.symlink/config.el
@@ -1,20 +1,90 @@
-(add-to-list 'default-frame-alist
-             '(ns-transparent-titlebar . t))
+;; My user information
+(setq user-full-name "Evan Riley"
+      user-mail-address "evanriley@hey.com")
+;; Change the Mac Modifiers to be better
+(cond (IS-MAC
+         (setq mac-command-modifier      'meta
+               mac-option-modifier       'alt
+               mac-right-option-modifier 'alt)))
 
-(add-to-list 'default-frame-alist
-             '(ns-appearance . dark))
+;; Enable auto-save and backup files
+(setq auto-save-default t
+      make-backup-files t)
 
+;; Set a nice looking splash image
+(let ((alternatives '("doomguy.png")))
+  ;; ((alternatives ( <some other stuff>)))
+  (setq fancy-splash-image
+        (concat doom-private-dir "misc/splash-images/"
+                (nth (random (length alternatives)) alternatives))))
+
+;; Set fonts
+(setq
+ doom-font (font-spec :family "JetBrains Mono" :size 16)
+ doom-big-font (font-spec :family "JetBrains Mono" :size 36)
+ doom-variable-pitch-font (font-spec :fammily "Overpass" :size 16)
+ doom-serif-font (font-spec :family "IBM Plex Mono" :weight 'light))
+
+;; Set theme
+(setq doom-theme 'doom-wilmersdorf)
+
+;; Modeline Settings
+(defun doom-modeline-conditional-buffer-encoding ()
+  (setq-local doom-modeline-buffer-encoding
+        (unless (and (memq (plist-get (coding-system-plist buffer-file-coding-system) :category)
+                           '(coding-category-undecided coding-category-utf-8))
+                     (not (memq (coding-system-eol-type buffer-file-coding-system) '(1 2))))
+          t)))
+
+(add-hook 'after-change-major-mode-hook #'doom-modeline-conditional-buffer-encoding)
+
+;; Relative line numbers
+(setq display-line-numbers-type 'relative)
+
+;; Buffer names
+(setq doom-fallback-buffer-name "► Doom"
+      +doom-dashboard-name "► Doom")
+
+;; Window title
+(setq frame-title-format
+      '(""
+        (:eval
+         (if (s-contains-p org-roam-directory (or buffer-file-name ""))
+             (replace-regexp-in-string
+              ".*/[0-9]*-?" "☰ "
+              (subst-char-in-string ?_ ? buffer-file-name))
+           "%b"))
+        (:eval
+         (let ((project-name (projectile-project-name)))
+           (unless (string= "-" project-name)
+             (format (if (buffer-modified-p) " ◉ %s" "  ●  %s") project-name))))))
+
+;; Simple Settings/Better Defaults
+(setq-default
+ delete-by-moving-to-trash t
+ window-combination-resize t
+ x-stretch-cursor t)
+
+(setq password-cache-expiry nil)
+
+(setq undo-limit 8000000
+      evil-want-fine-undo t)
+
+(display-time-mode 1)
+(global-subword-mode 1)
+
+;; Ask which buffer I want to see after splitting window.
 (setq evil-vsplit-window-right t
       evil-split-window-below t)
 
-(defadvice! prompt-for-buffer (&rest _)
+(defadvice! prompt-for-buffer (&rest)
   :after '(evil-window-split evil-window-vsplit)
   (+ivy/switch-buffer))
 
-(setq +ivy-buffer-preview t)
+(setq +ivy-buffer-preview
 
-
-(map! :map evil-window-map
+;; Use SPC w SPC for window rotation
+ (map! :map evil-window-map
       "SPC" #'rotate-layout
       ;; Navigation
       "<left>"     #'evil-window-left
@@ -25,252 +95,24 @@
       "C-<left>"       #'+evil/window-move-left
       "C-<down>"       #'+evil/window-move-down
       "C-<up>"         #'+evil/window-move-up
-      "C-<right>"      #'+evil/window-move-right)
+      "C-<right>"      #'+evil/window-move-right))
 
-(setq user-full-name "Evan Riley"
-      user-mail-address "evanriley@hey.com")
+;; Start Emacs Fullscreened
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
-(global-auto-revert-mode t)
-
-(add-hook 'org-mode #'auto-fill-mode)
-
-(add-hook! 'org-mode-hook (company-mode -1))
-(add-hook! 'org-capture-mode-hook (company-mode -1))
-
-
+;; Default Directories for Org and Org-Roam
 (setq
- doom-font (font-spec :family "JetBrains Mono" :size 16)
- doom-big-font (font-spec :family "JetBrains Mono" :size 36)
- doom-variable-pitch-font (font-spec :fammily "Overpass" :size 16)
- doom-serif-font (font-spec :family "IBM Plex Mono" :weight 'light)
- ;;doom-theme 'doom-oceanic-next
- ;;doom-theme 'doom-spacegrey
- ;;doom-theme 'doom-nord
- doom-theme 'doom-wilmersdorf
- web-mode-markup-indent-offset 2
- web-mode-code-indent-offset 2
- web-mode-css-indent-offset 2
- mac-command-modifier 'meta
- org-agenda-skip-scheduled-if-done t
- js-indent-level 2
- typescript-indent-level 2
- prettier-js-args '("--single-quote")
- projectile-project-search-path '("~/Code/")
- dired-dwim-target t
- org-ellipsis " ▾ "
- org-bullets-bullet-list '("·")
- org-tags-column -80
- org-agenda-files (ignore-errors (directory-files +org-dir t "\\.org$" t))
- org-log-done 'time
- css-indent-offset 2
- org-refile-target (quote ((nil :maxlevel . 1)))
- org-capture-templates '(("x" "Note" entry
-                          (file+olp+datetree "journal.org")
-                          "**** [ ] %U %?" :prepend t :kill-buffer t)
-                         ("t" "Task" entry
-                          (file+headline "tasks.org" "Inbox")
-                          "* [ ] %?\n%i" :prepend t :kill-buffer t))
- +org-capture-todo-file "tasks.org"
- org-super-agenda-groups '((:name "Today"
-                            :time-grid t
-                            :scheduled today)
-                           (:name "Due today"
-                            :deadline today)
-                           (:name "Important"
-                            :priority "A")
-                           (:name "Overdue"
-                            :deadline past)
-                           (:name "Due soon"
-                            :deadline future)
-                           (:name "Big Outcomes"
-                            :tag "bo")))
+ org-directory "~/Code/org/"
+ org-roam-directory "~/Code/org/notes")
 
-(setq doom-fallback-buffer-name "► Doom"
-      +doom-dashboard-name "► Doom")
+;; Hide Org Markup Indicators
+(after! org (setq org-hide-emphasis-markers t))
 
-(add-hook!
- js2-mode 'prettier-js-mode
- (add-hook 'before-save-hook #'refmt-before-save nil t))
+;; Insert Org Headings At Point.
+(after! org (setq org-insert-heading-respect-content nil))
 
-
-(map! :ne "M-/" #'comment-or-uncomment-region)
-(map! :ne "SPC / r" #'deadgrep)
-
-
-
-(after! org
-  (set-face-attribute 'org-link nil
-                      :weight 'normal
-                      :background nil)
-  (set-face-attribute 'org-code nil
-                      :foreground "#a9a1e1"
-                      :background nil)
-  (set-face-attribute 'org-date nil
-                      :foreground "#5B6268"
-                      :background nil)
-  (set-face-attribute 'org-level-1 nil
-                      :foreground "steelblue2"
-                      :background nil
-                      :height 1.2
-                      :weight 'normal)
-  (set-face-attribute 'org-level-2 nil
-                      :foreground "slategray2"
-                      :background nil
-                      :height 1.0
-                      :weight 'normal)
-  (set-face-attribute 'org-level-3 nil
-                      :foreground "SkyBlue2"
-                      :background nil
-                      :height 1.0
-                      :weight 'normal)
-  (set-face-attribute 'org-level-4 nil
-                      :foreground "DodgerBlue2"
-                      :background nil
-                      :height 1.0
-                      :weight 'normal)
-  (set-face-attribute 'org-level-5 nil
-                      :weight 'normal)
-  (set-face-attribute 'org-level-6 nil
-                      :weight 'normal)
-  (set-face-attribute 'org-document-title nil
-                      :foreground "SlateGray1"
-                      :background nil
-                      :height 1.75
-                      :weight 'bold)
-  (setq org-fancy-priorities-list '("⚡" "⬆" "⬇" "☕")))
-
-(setq-default
- delete-by-moving-to-trash t
- window-combination-resize t
- x-stretch-cursor t)
-
-(setq
- undo-limit 80000000
- evil-want-fine-undo t
- auto-save-default t)
-
-(display-time-mode 1)
-
-(setq password-cache-expiry nil)
-
-
-
-(after! web-mode
-  (add-to-list 'auto-mode-alist '("\\.njk\\'" . web-mode)))
-
-(setq +magit-hub-features t)
-
-(setq org-directory "~/Code/org/")
-(setq org-roam-directory "~/Code/org/notes")
-(setq org-directory "~/Code/org/")
-(setq org-roam-directory "~/Code/org/notes")
-
-(setq display-line-numbers-type t)
+;; Nested YASnippet
 (setq yas-triggers-in-field t)
 
-(setq rustic-lsp-server 'rust-analyzer)
-
-;; Cool splash screen, also stolen from tecosaur
-(defvar fancy-splash-image-template
-  (expand-file-name "misc/splash-images/blackhole-lines-template.svg" doom-private-dir)
-  "Default template svg used for the splash image, with substitutions from ")
-(defvar fancy-splash-image-nil
-  (expand-file-name "misc/splash-images/transparent-pixel.png" doom-private-dir)
-  "An image to use at minimum size, usually a transparent pixel")
-
-(setq fancy-splash-sizes
-      `((:height 500 :min-height 50 :padding (0 . 4) :template ,(expand-file-name "misc/splash-images/blackhole-lines-0.svg" doom-private-dir))
-        (:height 440 :min-height 42 :padding (1 . 4) :template ,(expand-file-name "misc/splash-images/blackhole-lines-0.svg" doom-private-dir))
-        (:height 400 :min-height 38 :padding (1 . 4) :template ,(expand-file-name "misc/splash-images/blackhole-lines-1.svg" doom-private-dir))
-        (:height 350 :min-height 36 :padding (1 . 3) :template ,(expand-file-name "misc/splash-images/blackhole-lines-2.svg" doom-private-dir))
-        (:height 300 :min-height 34 :padding (1 . 3) :template ,(expand-file-name "misc/splash-images/blackhole-lines-3.svg" doom-private-dir))
-        (:height 250 :min-height 32 :padding (1 . 2) :template ,(expand-file-name "misc/splash-images/blackhole-lines-4.svg" doom-private-dir))
-        (:height 200 :min-height 30 :padding (1 . 2) :template ,(expand-file-name "misc/splash-images/blackhole-lines-5.svg" doom-private-dir))
-        (:height 100 :min-height 24 :padding (1 . 2) :template ,(expand-file-name "misc/splash-images/emacs-e-template.svg" doom-private-dir))
-        (:height 0   :min-height 0  :padding (0 . 0) :file ,fancy-splash-image-nil)))
-
-(defvar fancy-splash-sizes
-  `((:height 500 :min-height 50 :padding (0 . 2))
-    (:height 440 :min-height 42 :padding (1 . 4))
-    (:height 330 :min-height 35 :padding (1 . 3))
-    (:height 200 :min-height 30 :padding (1 . 2))
-    (:height 0   :min-height 0  :padding (0 . 0) :file ,fancy-splash-image-nil))
-  "list of plists with the following properties
-  :height the height of the image
-  :min-height minimum `frame-height' for image
-  :padding `+doom-dashboard-banner-padding' to apply
-  :template non-default template file
-  :file file to use instead of template")
-
-(defvar fancy-splash-template-colours
-  '(("$colour1" . keywords) ("$colour2" . type) ("$colour3" . base5) ("$colour4" . base8))
-  "list of colour-replacement alists of the form (\"$placeholder\" . 'theme-colour) which applied the template")
-
-(unless (file-exists-p (expand-file-name "theme-splashes" doom-cache-dir))
-  (make-directory (expand-file-name "theme-splashes" doom-cache-dir) t))
-
-(defun fancy-splash-filename (theme-name height)
-  (expand-file-name (concat (file-name-as-directory "theme-splashes")
-                            theme-name
-                            "-" (number-to-string height) ".svg")
-                    doom-cache-dir))
-
-(defun fancy-splash-clear-cache ()
-  "Delete all cached fancy splash images"
-  (interactive)
-  (delete-directory (expand-file-name "theme-splashes" doom-cache-dir) t)
-  (message "Cache cleared!"))
-
-(defun fancy-splash-generate-image (template height)
-  "Read TEMPLATE and create an image if HEIGHT with colour substitutions as
-   described by `fancy-splash-template-colours' for the current theme"
-  (with-temp-buffer
-    (insert-file-contents template)
-    (re-search-forward "$height" nil t)
-    (replace-match (number-to-string height) nil nil)
-    (dolist (substitution fancy-splash-template-colours)
-      (goto-char (point-min))
-      (while (re-search-forward (car substitution) nil t)
-        (replace-match (doom-color (cdr substitution)) nil nil)))
-    (write-region nil nil
-                  (fancy-splash-filename (symbol-name doom-theme) height) nil nil)))
-
-(defun fancy-splash-generate-images ()
-  "Perform `fancy-splash-generate-image' in bulk"
-  (dolist (size fancy-splash-sizes)
-    (unless (plist-get size :file)
-      (fancy-splash-generate-image (or (plist-get size :file)
-                                       (plist-get size :template)
-                                       fancy-splash-image-template)
-                                   (plist-get size :height)))))
-
-(defun ensure-theme-splash-images-exist (&optional height)
-  (unless (file-exists-p (fancy-splash-filename
-                          (symbol-name doom-theme)
-                          (or height
-                              (plist-get (car fancy-splash-sizes) :height))))
-    (fancy-splash-generate-images)))
-
-(defun get-appropriate-splash ()
-  (let ((height (frame-height)))
-    (cl-some (lambda (size) (when (>= height (plist-get size :min-height)) size))
-             fancy-splash-sizes)))
-
-(setq fancy-splash-last-size nil)
-(setq fancy-splash-last-theme nil)
-(defun set-appropriate-splash (&rest _)
-  (let ((appropriate-image (get-appropriate-splash)))
-    (unless (and (equal appropriate-image fancy-splash-last-size)
-                 (equal doom-theme fancy-splash-last-theme)))
-    (unless (plist-get appropriate-image :file)
-      (ensure-theme-splash-images-exist (plist-get appropriate-image :height)))
-    (setq fancy-splash-image
-          (or (plist-get appropriate-image :file)
-              (fancy-splash-filename (symbol-name doom-theme) (plist-get appropriate-image :height))))
-    (setq +doom-dashboard-banner-padding (plist-get appropriate-image :padding))
-    (setq fancy-splash-last-size appropriate-image)
-    (setq fancy-splash-last-theme doom-theme)
-    (+doom-dashboard-reload)))
-
-(add-hook 'window-size-change-functions #'set-appropriate-splash)
-(add-hook 'doom-load-theme-hook #'set-appropriate-splash)
+;; Magit Settings
+(setq +magit-hub-features t)
