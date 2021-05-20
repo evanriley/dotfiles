d4e8623a08543e22e767a022ded5a0b1446df468
Evan Riley <evanriley@hey.com>
2021-05-20 12:00:55 -0400

    Vim keybind cleanup
    Still mostly going to use emacs though...I don't see that changing

--- vim/config+nvim.symlink/lua/general/keybindings/init.lua
+++ vim/config+nvim.symlink/lua/general/keybindings/init.lua
@@ -1,12 +1,9 @@
-local wk = require('which-key')
+local wk = require("which-key")
 
 -- Define leader key to space
 -- and call leader mapper
-Map('n', '<Space>', '<Nop>')
-Api.nvim_set_var('mapleader', ' ')
-Map('n', ',', '<Nop>')
-Api.nvim_set_var('maplocalleader', ',')
-
+Map("n", "<Space>", "<Nop>")
+Api.nvim_set_var("mapleader", " ")
 
 -------------------------------------------------
 
@@ -35,264 +32,274 @@ Api.nvim_set_var('maplocalleader', ',')
 -------------------------------------------------
 
 -- Additional options for mappings
-local opts = { silent = true }
+local opts = {silent = true}
 
 ---[[-----------------]]---
 --    LSP Keybindings    --
 ---]]-----------------[[---
 -- https://github.com/hrsh7th/nvim-compe#mappings
-	Map('i', '<expr> <C-Space>', Fn['compe#complete'](), opts)
-	Map('i', '<expr> <CR>', Fn['compe#confirm']('<CR>'), opts)
-	Map('i', '<expr> <C-e>', Fn['compe#close']('<C-e>'), opts)
-	Map(
-		'i',
-		'<expr> <C-f>',
-		Fn['compe#scroll']({ delta = '+4' }),
-		opts
-	)
-	Map(
-		'i',
-		'<expr> <C-d>',
-		Fn['compe#scroll']({ delta = '-4' }),
-		opts
-	)
-    	Map('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts) -- gd: jump to definitionA
-   	Map('n', 'gr', ':lua vim.lsp.buf.references()<CR>', opts) -- gr: go to reference
-   	Map('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', opts) -- gi: buf implementation
-   	Map('n', 'ca', ':Lspsaga code_action<CR>', opts) -- ca: code actions
-   	Map('n', 'K', ':Lspsaga hover_doc<CR>', opts) -- K: hover doc
-   	Map('n', '<C-p>', ':Lspsaga diagnostic_jump_prev<CR>', opts) -- Control+p: Jump to previous diagnostic
-   	Map('n', '<C-n>', ':Lspsaga diagnostic_jump_next<CR>', opts) -- Control+n: Jump to next diagnostic
-   	Map('n', '<C-f>', ':lua require("lspsaga.action").smart_scroll_with_saga(1)<CR>', opts) -- Control+f: Scroll down documents
-   	Map('n', '<C-b>', "lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>") -- Control+b: Scroll up documents
-   	Cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()')
+Map("i", "<expr> <C-Space>", Fn["compe#complete"](), opts)
+Map("i", "<expr> <CR>", Fn["compe#confirm"]("<CR>"), opts)
+Map("i", "<expr> <C-e>", Fn["compe#close"]("<C-e>"), opts)
+Map("i", "<expr> <C-f>", Fn["compe#scroll"]({delta = "+4"}), opts)
+Map("i", "<expr> <C-d>", Fn["compe#scroll"]({delta = "-4"}), opts)
+Map("n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts) -- gd: jump to definitionA
+Map("n", "gr", ":lua vim.lsp.buf.references()<CR>", opts) -- gr: go to reference
+Map("n", "gi", ":lua vim.lsp.buf.implementation()<CR>", opts) -- gi: buf implementation
+Map("n", "ca", ":Lspsaga code_action<CR>", opts) -- ca: code actions
+Map("n", "K", ":Lspsaga hover_doc<CR>", opts) -- K: hover doc
+Map("n", "<C-p>", ":Lspsaga diagnostic_jump_prev<CR>", opts) -- Control+p: Jump to previous diagnostic
+Map("n", "<C-n>", ":Lspsaga diagnostic_jump_next<CR>", opts) -- Control+n: Jump to next diagnostic
+Map("n", "<C-f>", ':lua require("lspsaga.action").smart_scroll_with_saga(1)<CR>', opts) -- Control+f: Scroll down documents
+Map("n", "<C-b>", "lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>") -- Control+b: Scroll up documents
+Cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()')
 
-Map('n', '<Leader>fn', ':new<CR>', opts)
+Map("n", "<Leader>fn", ":new<CR>", opts)
 
 -- TAB to cycle buffers too, why not?
-Map('n', '<Tab>', ':bnext<CR>', opts)
-Map('n', '<S-Tab>', ':bprevious<CR>', opts)
+Map("n", "<Tab>", ":bnext<CR>", opts)
+Map("n", "<S-Tab>", ":bprevious<CR>", opts)
 
 -- ESC to turn off search highlighting
-Map('n', '<esc>', ':noh<CR>', opts)
+Map("n", "<esc>", ":noh<CR>", opts)
 
 --- F<n> keybindings
-	Map('n', '<F2>', ':SymbolsOutline<CR>')
-	Map('n', '<F4>', ':ToggleTerm<CR>')
-	Map('n', '<F7>', ':DotHttp<CR>')
+Map("n", "<F2>", ":SymbolsOutline<CR>")
+Map("n", "<F4>", ":ToggleTerm<CR>")
+Map("n", "<F7>", ":DotHttp<CR>")
 ---[[------------------------------]]
 --     Window Movements keys      --
 ---]]------------------------------]]
 
-Map('n', '<C-h>', '<C-w>h', opts)
-Map('n', '<C-j>', '<C-w>j', opts)
-Map('n', '<C-k>', '<C-w>k', opts)
-Map('n', '<C-l>', '<C-w>l', opts)
+Map("n", "<C-h>", "<C-w>h", opts)
+Map("n", "<C-j>", "<C-w>j", opts)
+Map("n", "<C-k>", "<C-w>k", opts)
+Map("n", "<C-l>", "<C-w>l", opts)
 
 ---[[-----------------]]---
 --     Escape Remaps     --
 ---]]-----------------[[---
-Map('i', 'jk', '<ESC>', opts)
+Map("i", "jk", "<ESC>", opts)
 
 ---[[-----------------]]---
 --     Select Movement   --
 ---]]-----------------[[---
-Map ('x', 'K', ':move \'<-2<CR>gv-gv', opts)
-Map ('x', 'J', ':move \'>+1<CR>gv-gv', opts)
+Map("x", "K", ":move '<-2<CR>gv-gv", opts)
+Map("x", "J", ":move '>+1<CR>gv-gv", opts)
 
-Cmd('tnoremap <Esc> <C-\\><C-n>') -- get out of terminal insert mode into normal mode with Esc
+Cmd("tnoremap <Esc> <C-\\><C-n>") -- get out of terminal insert mode into normal mode with Esc
 
 ---[[-----------------]]---
 --     Resizing Splits   --
 ---]]-----------------[[---
-Cmd([[
+Cmd(
+    [[
   nnoremap <silent> <C-Up>    :resize -2<CR>
   nnoremap <silent> <C-Down>  :resize +2<CR>
   nnoremap <silent> <C-Right>  :vertical resize -2<CR>
   nnoremap <silent> <C-Left>  :vertical resize +2<CR>
-]])
+]]
+)
 
 ---[[-----------------]]---
 --     Disable keys      --
 ---]]-----------------[[---
 -- Disable accidentally pressing ctrl-z and suspending
-Map('n', '<c-z>', '<Nop>')
+Map("n", "<c-z>", "<Nop>")
 
 -- Disable ex mode
-Map('n', 'Q', '<Nop>')
+Map("n", "Q", "<Nop>")
 
 -- Disable recording
-Map('n', 'q', '<Nop>')
-
+Map("n", "q", "<Nop>")
 
 ---[[-----------------]]---
 --      Leader keys      --
 ---]]-----------------[[---
 -- Buffers
-wk.register({
-	['<leader>'] = {
-		b = {
-			name = '+buffer',
-			['1'] = { ':BufferGoto 1<CR>', 'Buffer 1' },
-			['2'] = { ':BufferGoto 2<CR>', 'Buffer 2' },
-			['3'] = { ':BufferGoto 3<CR>', 'Buffer 3' },
-			['4'] = { ':BufferGoto 4<CR>', 'Buffer 4' },
-			['5'] = { ':BufferGoto 5<CR>', 'Buffer 5' },
-			['6'] = { ':BufferGoto 6<CR>', 'Buffer 6' },
-			['7'] = { ':BufferGoto 7<CR>', 'Buffer 7' },
-			['8'] = { ':BufferGoto 8<CR>', 'Buffer 8' },
-			['9'] = { ':BufferLast<CR>', 'Last buffer' },
-			c = { ':BufferClose<CR>', 'Close buffer' },
-			f = { ':FormatWrite<CR>', 'Format buffer' },
-			n = { ':BufferNext<CR>', 'Next buffer' },
-			P = { ':BufferPick<CR>', 'Pick buffer' },
-			p = { ':BufferPrevious<CR>', 'Previous buffer' },
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            b = {
+                name = "+buffer",
+                ["1"] = {":BufferGoto 1<CR>", "Buffer 1"},
+                ["2"] = {":BufferGoto 2<CR>", "Buffer 2"},
+                ["3"] = {":BufferGoto 3<CR>", "Buffer 3"},
+                ["4"] = {":BufferGoto 4<CR>", "Buffer 4"},
+                ["5"] = {":BufferGoto 5<CR>", "Buffer 5"},
+                ["6"] = {":BufferGoto 6<CR>", "Buffer 6"},
+                ["7"] = {":BufferGoto 7<CR>", "Buffer 7"},
+                ["8"] = {":BufferGoto 8<CR>", "Buffer 8"},
+                ["9"] = {":BufferLast<CR>", "Last buffer"},
+                c = {":BufferClose<CR>", "Close buffer"},
+                f = {":FormatWrite<CR>", "Format buffer"},
+                n = {":BufferNext<CR>", "Next buffer"},
+                P = {":BufferPick<CR>", "Pick buffer"},
+                p = {":BufferPrevious<CR>", "Previous buffer"}
+            }
+        }
+    }
+)
 
 -- Plugins
-wk.register({
-	['<leader>'] = {
-		p = {
-			name = '+plugins',
-			c = { ':PackerClean<CR>', 'Clean disabled or unused plugins' },
-			i = { ':PackerInstall<CR>', 'Install missing plugins' },
-			p = {
-				':PackerProfile<CR>',
-				'Profile the time taken loading your plugins',
-			},
-			s = {
-				':PackerSync<CR>',
-				'Performs PackerClean and then PackerUpdate',
-			},
-			u = { ':PackerUpdate<CR>', 'Update your plugins' },
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            p = {
+                name = "+plugins",
+                c = {":PackerClean<CR>", "Clean disabled or unused plugins"},
+                i = {":PackerInstall<CR>", "Install missing plugins"},
+                p = {
+                    ":PackerProfile<CR>",
+                    "Profile the time taken loading your plugins"
+                },
+                s = {
+                    ":PackerSync<CR>",
+                    "Performs PackerClean and then PackerUpdate"
+                },
+                u = {":PackerUpdate<CR>", "Update your plugins"}
+            }
+        }
+    }
+)
 
 -- Order
-wk.register({
-	['<leader>'] = {
-		o = {
-			name = '+order',
-			d = { ':BufferOrderByDirectory<CR>', 'Sort by directory' },
-			l = { ':BufferOrderByLanguage<CR>', 'Sort by language' },
-			n = { ':BufferMoveNext<CR>', 'Re-order buffer to next' },
-			p = { ':BufferMovePrevious<CR>', 'Re-order buffer to previous' },
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            o = {
+                name = "+order",
+                d = {":BufferOrderByDirectory<CR>", "Sort by directory"},
+                l = {":BufferOrderByLanguage<CR>", "Sort by language"},
+                n = {":BufferMoveNext<CR>", "Re-order buffer to next"},
+                p = {":BufferMovePrevious<CR>", "Re-order buffer to previous"}
+            }
+        }
+    }
+)
 
 -- File
-wk.register({
-	['<leader>'] = {
-		f = {
-			name = '+file',
-			c = { ':e $MYVIMRC<CR>', 'Edit Neovim configuration' },
-			n = { 'Create a new unnamed buffer' },
-			f = { ':Telescope find_files<CR>', 'Find files' },
-			b = { ':Telescope marks<CR>', 'Bookmarks' },
-			W = { ':Telescope live_grep<CR>', 'Find word' },
-			t = { ':Telescope help_tags<CR>', 'Help tags' },
-			h = { ':Telescope oldfiles<CR>', 'Recently opened files' },
-			w = { ':SudaWrite<CR>', 'Write file with sudo permissions' },
-			r = { ':SudaRead<CR>', 'Re-open file with sudo permissions' },
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            f = {
+                name = "+file",
+                c = {":e $MYVIMRC<CR>", "Edit Neovim configuration"},
+                n = {"Create a new unnamed buffer"},
+                f = {":Telescope find_files<CR>", "Find files"},
+                b = {":Telescope marks<CR>", "Bookmarks"},
+                W = {":Telescope live_grep<CR>", "Find word"},
+                t = {":Telescope help_tags<CR>", "Help tags"},
+                h = {":Telescope oldfiles<CR>", "Recently opened files"},
+                w = {":SudaWrite<CR>", "Write file with sudo permissions"},
+                r = {":SudaRead<CR>", "Re-open file with sudo permissions"}
+            }
+        }
+    }
+)
 
 -- Window
-wk.register({
-	['<leader>'] = {
-		w = {
-			name = '+window',
-			C = { ':only<CR>', 'Close all other windows' },
-			c = { ':close<CR>', 'Close current window' },
-			h = { ':split<CR>', 'Split horizontally' },
-			v = { ':vsplit<CR>', 'Split vertically' },
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            w = {
+                name = "+window",
+                C = {":only<CR>", "Close all other windows"},
+                c = {":close<CR>", "Close current window"},
+                h = {":split<CR>", "Split horizontally"},
+                v = {":vsplit<CR>", "Split vertically"}
+            }
+        }
+    }
+)
 
 -- Sessions
-wk.register({
-	['<leader>'] = {
-		s = {
-			name = '+sessions',
-			s = { ':SaveSession<CR>', 'Save current session' },
-			r = { ':RestoreSession<CR>', 'Restore previously saved session' },
-			l = {
-				':Telescope session-lens search_session<CR>',
-				'Session switcher',
-			},
-		},
-	},
-})
-
+wk.register(
+    {
+        ["<leader>"] = {
+            s = {
+                name = "+sessions",
+                s = {":SaveSession<CR>", "Save current session"},
+                r = {":RestoreSession<CR>", "Restore previously saved session"},
+                l = {
+                    ":Telescope session-lens search_session<CR>",
+                    "Session switcher"
+                }
+            }
+        }
+    }
+)
 
 -- Toggler
-wk.register({
-	['<leader>'] = {
-		t = {
-			name = '+toggle',
-			s = { ':Dashboard<CR>', 'Open start screen' },
-			c = { ':DashboardChangeColorscheme<CR>', 'Change colorscheme' },
-			e = { ':NvimTreeToggle<CR>', 'Toggle Tree Explorer' },
-			S = { ':SymbolsOutline<CR>', 'Toggle Symbols view' },
-			t = { ':ToggleTerm<CR>', 'Toggle terminal' },
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            t = {
+                name = "+toggle",
+                s = {":Dashboard<CR>", "Open start screen"},
+                c = {":DashboardChangeColorscheme<CR>", "Change colorscheme"},
+                e = {":NvimTreeToggle<CR>", "Toggle Tree Explorer"},
+                S = {":SymbolsOutline<CR>", "Toggle Symbols view"},
+                t = {":ToggleTerm<CR>", "Toggle terminal"}
+            }
+        }
+    }
+)
 
-wk.register({
-	['<leader>'] = {
-		r = {
-			name = '+runner',
-			r = {
-				':DotHttp<CR>',
-				'Run restclient on the line that the cursor is currently on',
-			},
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            r = {
+                name = "+runner",
+                r = {
+                    ":DotHttp<CR>",
+                    "Run restclient on the line that the cursor is currently on"
+                }
+            }
+        }
+    }
+)
 
 -- LSP
-wk.register({
-	['<leader>'] = {
-		l = {
-			name = '+lsp',
-			d = {
-				'<cmd>lua vim.lsp.buf.type_definition()<CR>',
-				'Show type definition',
-			},
-			l = {
-				'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-				'Show line diagnostics',
-			},
-			L = {
-				'<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
-				'Diagnostics into location list',
-			},
-		},
-	},
-})
+wk.register(
+    {
+        ["<leader>"] = {
+            l = {
+                name = "+lsp",
+                d = {
+                    "<cmd>lua vim.lsp.buf.type_definition()<CR>",
+                    "Show type definition"
+                },
+                l = {
+                    "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",
+                    "Show line diagnostics"
+                },
+                L = {
+                    "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>",
+                    "Diagnostics into location list"
+                }
+            }
+        }
+    }
+)
 
--- Git 
-wk.register({
-	['<leader>'] = {
-		g = {
-			name = '+git',
-			o = { ':LazyGit<CR>', 'Open LazyGit' },
-			P = { ':TermExec cmd="git pull"<CR>', 'Pull' },
-			p = { ':TermExec cmd="git push"<CR>', 'Push' },
-			S = { 'Stage hunk' },
-			s = { ':TermExec cmd="git status"<CR>', 'Status' },
-			u = { 'Undo stage hunk' },
-			R = { 'Reset buffer' },
-			r = { 'Reset hunk' },
-			h = { 'Preview hunk' },
-			b = { 'Blame line' },
-		},
-	},
-})
+-- Git
+wk.register(
+    {
+        ["<leader>"] = {
+            g = {
+                name = "+git",
+                o = {":LazyGit<CR>", "Open LazyGit"},
+                P = {':TermExec cmd="git pull"<CR>', "Pull"},
+                p = {':TermExec cmd="git push"<CR>', "Push"},
+                S = {"Stage hunk"},
+                s = {':TermExec cmd="git status"<CR>', "Status"},
+                u = {"Undo stage hunk"},
+                R = {"Reset buffer"},
+                r = {"Reset hunk"},
+                h = {"Preview hunk"},
+                b = {"Blame line"}
+            }
+        }
+    }
+)
