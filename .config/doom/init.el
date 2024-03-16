57d0db4de12f25b6685ca6dc40d01dd90bdec7c1
Evan Riley <git@evanriley.dev>
2024-03-16 11:04:29 -0400

    Switch back to lsp-mode

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -48,7 +48,7 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
+       parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
@@ -84,8 +84,7 @@
        ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
-       (lsp               ; M-x vscode
-        +eglot)
+       (lsp)               ; M-x vscode
        (magit             ; a git porcelain for Emacs
         +forge)
        ;;make              ; run make tasks from Emacs
