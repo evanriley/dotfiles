13df053e3a4ae061c9a1ff1e2430327644b53507
Evan Riley <git@evanriley.dev>
2024-03-16 11:05:01 -0400

    Remove redundant keymap
    These keymaps are now handled by a plugin that also works with tmux.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -24,12 +24,6 @@ keymap.set("n", "<s-tab>", "<cmd>tabprev<Return>")
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
--- Better window navigation.
-keymap.set("n", "<C-h>", "<C-w>h")
-keymap.set("n", "<C-j>", "<C-w>j")
-keymap.set("n", "<C-k>", "<C-w>k")
-keymap.set("n", "<C-l>", "<C-w>l")
-
 -- Diagnostics
 keymap.set("n", "<C-j", function()
   vim.diagnostic.goto_next()
