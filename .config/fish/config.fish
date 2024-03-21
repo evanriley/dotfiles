ea10acc8e6dce6b3cc7565f9d34da42e9dc14f1b
Evan Riley <git@evanriley.dev>
2024-03-20 20:48:14 -0400

    cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 set -g fish_greeting
 set -gx EDITOR nvim
-set -gx PATH ~/bin ~/.local/bin $NPM_PACKAGES/bin ~/.roswell/bin ~/.yarn/bin ~/.cargo/bin ~/.config/emacs/bin /opt/homebrew/bin  /opt/homebrew/sbin ~/go/bin /opt/homebrew/opt/grep/libexec/gnubin '/Applications/Sublime Text.app/Contents/SharedSupport/bin' /home/evan/.local/share/bob/nvim-bin $PATH
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx PATH ~/bin ~/.local/bin $NPM_PACKAGES/bin ~/.roswell/bin ~/.yarn/bin ~/.cargo/bin ~/.config/emacs/bin /opt/homebrew/bin /opt/homebrew/sbin ~/go/bin /opt/homebrew/opt/grep/libexec/gnubin '/Applications/Sublime Text.app/Contents/SharedSupport/bin' /home/evan/.local/share/bob/nvim-bin $PATH
 set -gx GPG_TTY (tty)
 
 # Aliases
-alias vim 'nvim'
-alias e  'run-emacs'
+alias vim nvim
+alias e run-emacs
 
 ## get rid of command not found
 alias cd.. 'cd ..'
@@ -22,16 +23,16 @@ alias .5 'cd ../../../../..'
 alias myip "curl -6 https://ifconfig.co; echo"
 
 ## more util commands
-alias ls 'eza'
-alias cat 'bat'
-alias du 'dust'
+alias ls eza
+alias cat bat
+alias du dust
 
 ## fast clj repl
 alias clj-repl 'clj "-J-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl :server-daemon false}"'
 
 # use pinentry-mac instead of pinentry on macos
-if test (uname) = "Darwin"
-  alias pinentry 'pinentry-mac'
+if test (uname) = Darwin
+    alias pinentry pinentry-mac
 end
 
 # mise is an asdf replacement
