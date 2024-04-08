6813270ec96ba04ddea8cf820818c3668b6d6b09
Evan Riley <git@evanriley.dev>
2024-04-08 17:46:37 -0400

    nvim:smart-splits to move between nvim and wezterm

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -12,7 +12,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
 
 -- Keybinds to make split navigation easier.
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
