ece068220d2c18e6fd7621c8700ccd277dec2ac6
Evan Riley <git@evanriley.dev>
2024-04-08 16:54:58 -0400

    nvim: ensure gofumpt is installed by mason

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -75,6 +75,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'gofumpt', -- Superior go formatter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
       require('mason-lspconfig').setup {
