3585d0cd34598e55dc102171ac84d3c4b768ef24
Evan Riley <git@evanriley.dev>
2024-04-08 21:57:50 -0400

    nvim:switch to nightfox themes, change with system

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,16 +1,20 @@
 return {
   {
-    'rebelot/kanagawa.nvim',
+    'f-person/auto-dark-mode.nvim',
+    dependencies = {
+      'EdenEast/nightfox.nvim',
+    },
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      require('kanagawa').setup {
-        undercurl = false,
-        commentStyle = { italic = false },
-        keywordStyle = { italic = false },
-      }
-    end,
-    init = function()
-      vim.cmd.colorscheme 'kanagawa'
-    end,
+    config = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value('background', 'dark', {})
+        vim.cmd 'colorscheme duskfox'
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value('background', 'light', {})
+        vim.cmd 'colorscheme dayfox'
+      end,
+    },
   },
 }
