1f2b3f28f35539d9c85ef8375eac91789ae44316
Evan Riley <git@evanriley.dev>
2024-04-12 23:00:21 -0400

    nvim: simplify clojure setup

--- .config/nvim/lua/plugins/conjure.lua
+++ .config/nvim/lua/plugins/conjure.lua
@@ -1,23 +1,29 @@
 return {
   {
     'Olical/conjure',
+    dependencies = {
+      'clojure-vim/vim-jack-in',
+      'tpope/vim-dispatch',
+      'radenling/vim-dispatch-neovim',
+    },
     ft = { 'clojure', 'fennel' },
     config = function(_, opts)
       require('conjure.main').main()
       require('conjure.mapping')['on-filetype']()
     end,
     init = function()
-      -- vim.g['conjure#log#hud#width'] = 1
-      -- vim.g['conjure#log#hud#enabled'] = false
-      -- vim.g['conjure#log#hud#anchor'] = 'SE'
-      -- vim.g['conjure#log#botright'] = true
-      -- vim.g['conjure#extract#context_header_lines'] = 100
-      -- vim.g['conjure#eval#comment_prefix'] = ';; '
-      -- vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = false
-      -- vim.g['conjure#client#clojure#nrepl#connection#auto_repl#hidden'] = true
-      -- vim.g['conjure#client#clojure#nrepl#connection#auto_repl#cmd'] = nil
-      -- vim.g['conjure#client#clojure#nrepl#eval#auto_require'] = false
-      -- vim.g['conjure#client#clojure#nrepl#test#runner'] = 'kaocha'
+      vim.g['conjure#extract#context_header_lines'] = 100
+      vim.g['conjure#eval#comment_prefix'] = ';; '
+      -- Defer the setting of conjure#client#clojure#nrepl#connection#auto_repl#cmd
+      vim.api.nvim_create_autocmd('FileType', {
+        desc = 'Wait until a clojure file is opened to set this.', -- Look, I don't know what I'm doing but I do know that this works...
+        pattern = { 'clojure' },
+        callback = function()
+          vim.g['conjure#client#clojure#nrepl#connection#auto_repl#cmd'] = vim.fn['jack_in#clj_cmd']()
+        end,
+      })
+      vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = true
+      vim.g['conjure#client#clojure#nrepl#eval#auto_require'] = true
     end,
   },
 }
