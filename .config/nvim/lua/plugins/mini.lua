de8770b179502686082ae0e33401e8b55d45218b
Evan Riley <git@evanriley.dev>
2024-04-14 22:26:35 -0400

    nvim: fix mini.bufremove keymaps

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,31 +1,6 @@
 return {
   {
     'echasnovski/mini.nvim',
-    keys = {
-      {
-        '<leader>bd',
-        function()
-          local bd = require('mini.bufremove').delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm(('Save changes to %q?'):format(vim.fn.bufname()), '&Yes\n&No\n&Cancel')
-            if choice == 1 then -- Yes
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then -- No
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-      },
-      {
-        '<leader>bD',
-        function()
-          require('mini.bufremove').delete(0, true)
-        end,
-      },
-    },
     config = function()
       -- autopairs
       require('mini.pairs').setup()
@@ -60,7 +35,25 @@ return {
         return '%2l:%-2v'
       end
       -- remove buffers
-      require('mini.bufremove').setup()
+      require('mini.bufremove').setup {
+        vim.keymap.set('n', '<leader>bd', function()
+          local bd = require('mini.bufremove').delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(('Save changes to %q?'):format(vim.fn.bufname()), '&Yes\n&No\n&Cancel')
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end),
+        vim.keymap.set('n', '<leader>bd', function()
+          require('mini.bufremove').delete(0, true)
+        end),
+      }
     end,
   },
 }
